73eb4567d760523adbca28c5bb1e0288d6124030
Risto Lankinen <rlankine@gmail.com>
2021-03-22 14:20:23 +0200

    Tekstaus framework mostly finished; Validation, testing, and fine-tuning will follow.

--- main.cpp
+++ main.cpp
@@ -20,13 +20,23 @@ String Reverse2(String const& r)
 
 int main() try
 {
+	String s('x', 99);
+	cout << s << endl;
+
+#if 0
+	String a = String('<', 1);
+	String b = String('<', 4);
+
+	cout << (a + b).Head(3) << "|" << (a + b).Tail(3) << endl;
+
 	String HW = "HelloWorld";
-	String greeting = HW.Head(5) + ", " + HW.Tail(5) + "!";
+	String greeting = String('>',5) + HW.Head(5) + ", " + HW.Tail(5) + "!" + String('<',5);
 	cout << greeting << endl;
 	greeting = Reverse1(greeting);
 	cout << greeting << endl;
 	greeting = Reverse2(greeting);
 	cout << greeting << endl;
+#endif
 	return EXIT_SUCCESS;
 }
 catch (char const* p)
